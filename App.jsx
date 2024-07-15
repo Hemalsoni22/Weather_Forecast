12dffc880384c41ff12bfeb98d3c08ae1b9aab3c
Hemal Soni <sonisoni220604@gmail.com>
2024-07-15 18:10:50 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Ui from "./components/Ui";
+
+const App = () => {
+  return (
+    <>
+      {/* <Header name="hello world" enrol={211040107040} /> */}
+      <Ui></Ui>
+    </>
+  );
+};
+
+export default App;
